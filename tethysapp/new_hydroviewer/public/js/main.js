184dc3e5081ec35b8fa55ed9e833e43e1777c7a9
romer8 <gio.busrom@gmail.com>
2022-10-14 14:52:04 -0600

    added new changes for the forecast table

--- tethysapp/new_hydroviewer/public/js/main.js
+++ tethysapp/new_hydroviewer/public/js/main.js
@@ -13,17 +13,18 @@ var HYDROVIEWER_PACKAGE = (function() {
               zoom: 2
             })
           });
-        map_object = new Map()
+        map_object = new Map_Object()
         map_object.add_base_layers_map(map);
         console.log(geoserver_endpoint,streams_layer_name,stations_layer_name);
         map_object.add_streams_and_stations(map,geoserver_endpoint,streams_layer_name,stations_layer_name);
         map_object.fit_view_streams_wms(map,geoserver_endpoint,geoserver_workspace,streams_layer_name);
-        map_object.create_wms_events_popup(map,[map_object.getLayersObject().get_streams_wms()],'ecmwf-get-time-series/');
-        // map_object.create_wms_events_popup(map,[map_object.getLayersObject().get_streams_wms()],'ecmwf-get-time-series/','get-historic-data/');
-
-
+        map_object.generate_events_map();
+        // map_object.create_wms_events_popup(map,[map_object.getLayersObject().get_streams_wms()],'ecmwf-get-time-series/');
+        map_object.create_wms_events_popup(map,[map_object.getLayersObject().get_streams_wms()],'ecmwf-get-time-series/','get-historic-data/','get-flow-duration-curve/','get-daily-seasonal-streamflow/','get-monthly-seasonal-streamflow/','get-forecast-percent/');
         get_warning_points_local(map);
-        console.log(model)   
+        map_object.generate_events_map();
+
+        // console.log(model)   
         
     }
     var get_warning_points_local = function(map){
@@ -91,18 +92,18 @@ function map_events(wms_layers) {
             
             $("#graph").modal('show');
             $("#tbody").empty()
-            $('#long-term-chart').addClass('hidden');
-            $('#historical-chart').addClass('hidden');
-            $('#fdc-chart').addClass('hidden');
-            $('#seasonal_d-chart').addClass('hidden');
-            $('#seasonal_m-chart').addClass('hidden');
-            $('#download_forecast').addClass('hidden');
-            $('#download_era_5').addClass('hidden');
+            $('#long-term-chart').addClass('d-none');
+            $('#historical-chart').addClass('d-none');
+            $('#fdc-chart').addClass('d-none');
+            $('#seasonal_d-chart').addClass('d-none');
+            $('#seasonal_m-chart').addClass('d-none');
+            $('#download_forecast').addClass('d-none');
+            $('#download_era_5').addClass('d-none');
 
-            $loading.removeClass('hidden');
+            $loading.removeClass('d-none');
             //Retrieving the details for clicked point via the url
-            $('#dates').addClass('hidden');
-            //$('#plot').addClass('hidden');
+            $('#dates').addClass('d-none');
+            //$('#plot').addClass('d-none');
             $.ajax({
                 type: "GET",
                 url: wms_url,
@@ -135,7 +136,7 @@ function map_events(wms_layers) {
 
                     var workspace = geoserver_workspace;
 
-                    $('#info').addClass('hidden');
+                    $('#info').addClass('d-none');
                     add_feature(model, workspace, comid);
 
                 },
@@ -262,81 +263,5 @@ function add_feature(model, workspace, comid) {
 
 
 $(function() {
-    // $('#app-content-wrapper').removeClass('show-nav');
-    // $(".toggle-nav").removeClass('toggle-nav');
 
-    // //make sure active Plotly plots resize on window resize
-    // window.onresize = function() {
-    //     $('#graph .modal-body .tab-pane.active .js-plotly-plot').each(function(){
-    //         Plotly.Plots.resize($(this)[0]);
-    //     });
-    // };
-
-    // init_map();
-
-
-    // map_events();
-    // submit_model();
-    // resize_graphs();
-    // // If there is a defined Watershed, then lets render it and hide the controls
-    // let ws_val = $('#watershed').find(":selected").text();
-    // if (ws_val && ws_val !== 'Select Watershed') {
-    //     console.log("hola")
-    //     view_watershed();
-    //     $("[name='update_button']").hide();
-    // }
-    // else{
-    //     console.log("Iam here")
-    //     view_watershed();
-
-    // }
-
-    // // If there is a button to save default WS, let's add handler
-    // $("[name='update_button']").click(() => {
-    //     $.ajax({
-    //         url: 'admin/setdefault',
-    //         type: 'GET',
-    //         dataType: 'json',
-    //         data: {
-    //             'ws_name': $('#model').find(":selected").text(),
-    //             'model_name': $('#watershed').find(":selected").text()
-    //         },
-    //         success: function() {
-    //             // Remove the set default button
-    //             $("[name='update_button']").hide(500);
-    //             console.log('Updated Defaults Successfully');
-    //         }
-    //     });
-    // })
-
-
-    // $('#datesSelect').change(function() { //when date is changed
-    // 	//console.log($("#datesSelect").val());
-
-    //     //var sel_val = ($('#datesSelect option:selected').val()).split(',');
-    //     sel_val = $("#datesSelect").val()
-
-    //     //var startdate = sel_val[0];
-    //     var startdate = sel_val;
-    //     startdate = startdate.replace("-","");
-    //     startdate = startdate.replace("-","");
-
-    //     //var watershed = sel_val[1];
-    //     var watershed = 'south_america';
-
-    //     //var subbasin = sel_val[2];
-    //     var subbasin = 'geoglows';
-
-    //     //var comid = sel_val[3];
-    //     var model = 'ECMWF-RAPID';
-
-    //     $loading.removeClass('hidden');
-    //     get_time_series(model, watershed, subbasin, comid, startdate);
-    //     get_forecast_percent(watershed, subbasin, comid, startdate);
-    // });
 });
-
-
-
-// // Regions gizmo listener
-// $('#regions').change(function() {getRegionGeoJsons()});
